939a27a1c274d3bc5b4bf75ba036708d5a139189
Joshywaaa <69896606+Joshywaaa@users.noreply.github.com>
2023-10-24 10:30:10 +1300

    head

--- gallery.php
+++ gallery.php
@@ -1,20 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <!-- Set character encoding and viewport for responsive design -->
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <!-- Link to the external CSS stylesheet -->
-    <link rel="stylesheet" href="css/style.css">
-    
-    <!-- Include Bootstrap CSS and JavaScript using CDNs -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    
-    <!-- Title of the webpage -->
-    <title>Total Textures</title>
-</head>
+<?php include "includes/head.php" ?>
+
 <body>
     <!-- Navigation bar with Bootstrap styling -->
     <?php include "includes/nav.php"?>
--- index.php
+++ index.php
@@ -1,24 +1,6 @@
 <!DOCTYPE html>
 <html>
-
-<head>
-    <!-- Set character encoding and viewport for responsive design -->
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <!-- Link to the external CSS stylesheet -->
-    <link rel="stylesheet" href="css/style.css">
-
-    <!-- Include Bootstrap CSS using a CDN -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    
-    <!-- Include Bootstrap JavaScript and Popper.js using CDNs -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-
-    <!-- Set the title of the webpage -->
-    <title>Total Textures</title>
-</head>
+<?php include "includes/head.php" ?>
 
 <body>
     <!-- Navigation bar with Bootstrap styling -->
